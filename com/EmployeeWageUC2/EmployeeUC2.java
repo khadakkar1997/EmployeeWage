dcb21c88bb8e99140cb2e56230c3e378447e16ea
arya <aryakhadakkar03@gmail.com>
2021-05-13 17:41:55 +0530

    welcome to Employee Wage

---
+++ com/EmployeeWageUC2/EmployeeUC2.java
@@ -0,0 +1,15 @@
+package com.EmployeeWageUC2;
+
+public class EmployeeUC2 {
+	
+	public static void main(String[] args) {
+		// Constants
+		int IS_PART_TIME = 1;
+		
+		
+		
+		
+		System.out.println("");
+	}
+
+}
